b522080dfe5d667bb8e255106229ab5d728fd292
MrinalS24 <connectwithmrinaal@gmail.com>
2025-05-10 12:02:13 +0200

    Add files via upload

--- Fruit Order/main.py
+++ Fruit Order/main.py
@@ -1,10 +1,21 @@
 import pgzrun
 import random
+import time
+#import kivy
+
+#from kivy import Clock
+
 
 WIDTH = 867
 HEIGHT = 550
 
 fruits = []
+lines = []
+
+starttime = 0
+totaltime = 0
+
+nextfruit = 0
 
 apple = Actor("apple")
 fruits.append(apple)
@@ -30,9 +41,23 @@ fruits.append(watermelon)
 for fruit in fruits:
     fruit.pos = (random.randint(60,800),random.randint(70,500))
 
+
 random.shuffle(fruits)
 
+starttime = time.time()
+counter = 0
+timecount = 5
+
+def timer():
+    global counter
+    if timecount == 0:
+        counter =+ 1
+    timecount -= 1
+
+
+
 def draw():
+    global totaltime
     screen.clear()
     screen.fill(color = "pink")
     screen.blit("computer_screen",(0,0))
@@ -41,6 +66,37 @@ def draw():
         i.draw()
         screen.draw.text(str(number), (i.pos[0], i.pos[1]+ 20))
         number += 1
+    for m in lines:
+        screen.draw.line(m[0],m[1], (255,255,255))
+    """if nextfruit < len(fruits):
+        totaltime = time.time() - starttime
+        screen.draw.text(str(round(totaltime, 1)), (25,25), color = "white")
+    else:
+        screen.draw.text(str(round(totaltime, 1)), (25,25), color = "white")"""
+    
+    if nextfruit < len(fruits):
+        timer()
+        screen.draw.text(str(counter),(25,25),color = "white" )
+
+
+    
+
+
+
+def on_mouse_down(pos):
+    global lines, nextfruit
+    if nextfruit < len(fruits):
+        if fruits[nextfruit].collidepoint(pos):
+            if nextfruit:
+                lines.append((fruits[nextfruit-1].pos,fruits[nextfruit].pos))
+            nextfruit += 1
+        else:
+            lines = []
+            nextfruit = 0
+
+
+#Clock.schedule_interval(timer,5)
+
 
 
 
